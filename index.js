fbc2192a8a3d9f466b56ee3cc066861e72eef3f6
Reece Crook <reececrook16@gmail.com>
2023-03-13 19:58:29 -0600

    Completed lab

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(amount){
   // Write your code here!
+  if (amount <= 400){
+    return 'This one is on me!'
+  }
+  else if (amount >= 400){
+    if (amount <= 2000){
+      return 'That will be twenty bucks.'
+    }
+  }
+  else (amount >= 2000);{
+    if (amount >= 2500){
+      return 'No can do.'
+    }
+    return 'I will gladly take your thirty bucks.'
+  }
+  
+
+}
+scuberGreetingForFeet(199)
+
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  return (city === 'NYC' ? 'Ok, sounds good.' : 'No go.')
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case "generous":
+      return "Thank you so much."
+      break;
+    case "not as generous":
+      return "Thank you."
+      break;
+    default:
+      return "Bye."
+      break;
+  }
+
+
 }
